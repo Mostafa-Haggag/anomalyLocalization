4dc829138a3d31b66fd7daddbe3d2753fb9bfb10
mostafa.haggag <mostafa.haggag@aiviz.it>
2024-06-18 15:37:55 +0200

    Added Dataloader for the test set to be able to test the unet and compare to the gn results

--- dataset.py
+++ dataset.py
@@ -43,15 +43,15 @@ class MVTecAD(data.Dataset):
         image_path = self.image_paths[index]
         #image = Image.open(image_path).convert('L')  # Convert to grayscale
         image = Image.open(image_path)
-        # opencv_image = np.array(image)
-        #
-        # opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_RGB2BGR)
-        # gaussian_blurred = cv2.GaussianBlur(opencv_image, (15, 15), 0)
-        # enhanced_image = cv2.convertScaleAbs(gaussian_blurred, alpha=1.5, beta=0)
-        # enhanced_image_rgb = cv2.cvtColor(enhanced_image, cv2.COLOR_BGR2RGB)
-        # image = Image.fromarray(enhanced_image_rgb)
+        opencv_image = np.array(image)
 
-        #image = Image.open(image_path)  # not gray scale as picture is colored
+        opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_RGB2BGR)
+        gaussian_blurred = cv2.GaussianBlur(opencv_image, (15, 15), 0)
+        enhanced_image = cv2.convertScaleAbs(gaussian_blurred, alpha=1.5, beta=0)
+        enhanced_image_rgb = cv2.cvtColor(enhanced_image, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(enhanced_image_rgb)
+
+        image = Image.open(image_path)  # not gray scale as picture is colored
 
         if self.transform is not None:
             image = self.transform(image)
@@ -60,6 +60,74 @@ class MVTecAD(data.Dataset):
     def __len__(self):
         """Return the number of images."""
         return len(self.image_paths)
+
+
+def get_sorted_file_paths(image_dir, mask_dir, extensions):
+    """
+    Returns a sorted list of tuples (image_path, mask_path) ensuring that filenames match.
+    """
+    image_files = [file for file in os.listdir(image_dir) if file.endswith(extensions)]
+    mask_files = [file for file in os.listdir(mask_dir) if file.endswith(extensions)]
+
+    # Sort the lists
+    image_files.sort()
+    mask_files.sort()
+
+    # Ensure the number of images and masks match
+    if len(image_files) != len(mask_files):
+        raise ValueError("The number of images and masks do not match!")
+
+    # Create tuples of image and mask paths, ensuring they have matching filenames
+    paired_paths = []
+    for image_file, mask_file in zip(image_files, mask_files):
+        if image_file.split('.')[0] != mask_file.split('.')[0]:
+            raise ValueError(f"Mismatched filenames: {image_file} and {mask_file}")
+        image_path = os.path.join(image_dir, image_file)
+        mask_path = os.path.join(mask_dir, mask_file)
+        paired_paths.append((image_path, mask_path))
+
+    return paired_paths
+
+
+
+class MVTecAD_test_set_with_masks(data.Dataset):
+    """Dataset class for the MVTecAD dataset."""
+
+    def __init__(self, image_dir, transform):
+        """Initialize and preprocess the MVTecAD dataset."""
+        self.image_dir = image_dir
+        self.transform = transform
+        _extensions = ('.jpg', '.bmp', '.png')
+        mask_dir = f"{image_dir}_mask"
+        self.image_masks_paths = get_sorted_file_paths(image_dir, mask_dir, _extensions)
+
+    def __getitem__(self, index):
+        """Return one image"""
+
+        image_path, mask_path = self.image_masks_paths[index]
+        # This is the mask
+        # mask_path = self.mask_paths[index]
+
+        #image = Image.open(image_path).convert('L')  # Convert to grayscale
+        image = Image.open(image_path)
+        mask = Image.open(mask_path).convert('L')
+        # Step 2: Convert to NumPy array
+        mask_np = np.array(mask)
+
+        # Step 3: Apply threshold to binarize the mask
+        binarized_mask_np = ((mask_np > 0).astype(np.uint8)*255).astype(np.uint8)
+
+        # Step 4: Convert back to PIL Image (optional)
+        mask = Image.fromarray(binarized_mask_np)
+        if self.transform is not None:
+            image = self.transform(image)
+            mask = self.transform(mask)
+
+        return image,mask
+
+    def __len__(self):
+        """Return the number of images."""
+        return len(self.image_masks_paths)
 
 
 def return_MVTecAD_loader(image_dir, batch_size=256, train=True):
@@ -73,7 +141,7 @@ def return_MVTecAD_loader(image_dir, batch_size=256, train=True):
     crop_size = (400, 1936)  # Desired crop size
     #CenterCrop(crop_size)
     #transform.append(CenterCrop(crop_size))
-    transform.append(T.Resize((64, 64)))
+    transform.append(T.Resize((224, 224)))
 
     #transform.append(T.RandomCrop((128,128)))
     #transform.append(T.RandomHorizontalFlip(p=0.5))
@@ -88,6 +156,37 @@ def return_MVTecAD_loader(image_dir, batch_size=256, train=True):
                                   batch_size=batch_size,
                                   shuffle=train,
                                   drop_last=True,
+                                  num_workers=8,
+                                  pin_memory=True)
+    return data_loader
+
+
+def return_MVTecAD_loader_test_GN(image_dir, batch_size=256):
+    """Build and return a data loader."""
+    transform = []
+    # mean = [0.5, 0.5, 0.5]  # (assuming grayscale or RGB)
+    # std = [0.5, 0.5, 0.5]  # (assuming grayscale or RGB)
+    mean = [0.0622145,  0.0864737,  0.07538847]
+    std = [0.09039213, 0.08525948, 0.09549119]
+    # Desired output size of the crop
+    crop_size = (400, 1936)  # Desired crop size
+    #CenterCrop(crop_size)
+    #transform.append(CenterCrop(crop_size))
+    transform.append(T.Resize((224, 224)))
+
+    #transform.append(T.RandomCrop((128,128)))
+    #transform.append(T.RandomHorizontalFlip(p=0.5))
+    #transform.append(T.RandomVerticalFlip(p=0.5))
+    transform.append(T.ToTensor())
+    # transform.append(T.Normalize(mean, std))
+    transform = T.Compose(transform)
+
+    dataset = MVTecAD_test_set_with_masks(image_dir, transform)
+
+    data_loader = data.DataLoader(dataset=dataset,
+                                  batch_size=batch_size,
+                                  shuffle=False,
+                                  drop_last=False,
                                   num_workers=8,
                                   pin_memory=True)
     return data_loader
---
+++ test_unet_gn.py
@@ -0,0 +1,378 @@
+
+from network import VAE_new
+import torch
+import random
+from dataset import return_MVTecAD_loader,return_MVTecAD_loader_test_GN
+from kornia.filters import gaussian_blur2d
+
+from tqdm import tqdm
+import numpy as np
+import cv2
+from unet import UNetModel,UNetModel_noskipp
+from torchvision.transforms import transforms
+from scipy.ndimage import gaussian_filter
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import confusion_matrix
+
+def makeVideoFromImageArray(output_filename, image_list):
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    # Defines the codec to be used for the video. XVID is a popular video codec.
+
+    image_width = image_list[0].shape[1]
+    image_height = image_list[0].shape[0]
+    # Retrieves the width and height of the images from the first image in the list.
+    # It assumes all images are of the same size.
+
+    out = cv2.VideoWriter(filename=output_filename, fourcc=fourcc, fps=8,
+                          frameSize=(image_width, image_height), isColor=True)
+    # loop over the list of pictures
+    for image_number, image in enumerate(image_list, 1):
+        # Create a text overlay with the image number
+        text = f"Frame {image_number}"
+        font = cv2.FONT_HERSHEY_SIMPLEX # Chooses the font FONT_HERSHEY_SIMPLEX.
+        font_scale = 0.5
+        font_thickness = 1
+        # Sets the font scale to 0.5 and thickness to 1.
+        text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+        text_x = 10
+        text_y = 20
+
+        # Add the text to the frame
+        frame_with_text = image.copy()
+
+        # Add the text to the frame
+        # Uses cv2.putText to overlay the text on the image.
+        # cv2.putText(frame_with_text, text, (text_x, text_y), font,
+        #             font_scale, (255, 255, 255), font_thickness)
+
+        out.write(frame_with_text)
+
+    out.release()
+
+def makeVideoFromImageArray_uncolored(output_filename, image_list):
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    image_height, image_width = image_list[0].shape[:2]
+
+    out = cv2.VideoWriter(filename=output_filename, fourcc=fourcc, fps=8,
+                          frameSize=(image_width, image_height), isColor=False)
+
+    for image_number, image in enumerate(image_list, 1):
+        # # Convert single-channel image to 3-channel (if needed)
+        # if len(image.shape) == 2:
+        #     # image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+        #     image
+        # Add text overlay
+        text = f"Frame {image_number}"
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 0.5
+        font_thickness = 1
+        text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+        text_x = 10
+        text_y = 20
+        # cv2.putText(image, text, (text_x, text_y), font, font_scale, 255, font_thickness)
+
+        out.write(image)
+
+    out.release()
+# def convert_to_grayscale(image):
+#     """
+#     Convert an image to grayscale.
+#     """
+#     return cv2.cvtColor( cv2.cvtColor(image, cv2.COLOR_RGB2BGR), cv2.COLOR_BGR2GRAY)
+# def convert_to_grayscale(image):
+#     """
+#     Convert a NumPy array of shape (3, H, W) representing an RGB image to grayscale.
+#     """
+#     # Ensure the input image is in the format (H, W, 3) for easier manipulation
+#     image = np.transpose(image, (1, 2, 0))  # Convert to (H, W, 3)
+#
+#     # Apply the grayscale conversion weights
+#     gray_image = np.dot(image[..., :3], [0.2989, 0.5870, 0.1140])
+#
+#     return gray_image
+def convert_to_grayscale(image):
+    """
+    Convert an RGB image to grayscale.
+    """
+    # Ensure the input image is in the format (H, W, 3) for easier manipulation
+    image = np.transpose(image, (1, 2, 0))  # Convert to (H, W, 3)
+    return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+def mse(actual, pred):
+    actual, pred = np.array(actual), np.array(pred)
+    return np.square(np.subtract(actual,pred))
+
+def plot_bounding_boxes(original, reconstructed, threshold=0.1):
+    """
+    Plot bounding boxes around areas with significant differences.
+    """
+    # difference =mse(original,reconstructed)
+    difference = np.abs(original - reconstructed)
+    #difference_gray = np.mean(difference, axis=0)
+    difference_gray = np.sum(difference, axis=0)
+    #difference_gray = np.max(difference, axis=0)
+    mask = ((difference_gray > threshold).astype(np.uint8) * 255).astype(np.uint8)
+
+
+
+    # Convert original and reconstructed to grayscale
+    # original_gray = convert_to_grayscale(original)
+    # reconstructed_gray = convert_to_grayscale(reconstructed)
+
+    # Compute the absolute difference
+    # difference_gray = np.abs(original_gray - reconstructed_gray)
+
+    # Threshold the difference
+    # mask = (difference_gray > threshold ).astype(np.uint8)
+
+    # Ensure the mask is in the correct format (binary image)
+    if mask is None or mask.size == 0:
+        raise ValueError("The mask image is empty or not valid.")
+    if len(mask.shape) != 2 or mask.dtype != np.uint8:
+        raise ValueError("The mask image must be a binary image of type np.uint8.")
+
+
+
+    # Find contours
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Convert the original image to BGR format for drawing rectangles
+    original_bgr = np.transpose(original, (1, 2, 0))  # Convert to (H, W, 3)
+    original_bgr = (original_bgr * 255).astype(np.uint8)  # Scale to [0, 255]
+    original_bgr = cv2.cvtColor(original_bgr, cv2.COLOR_RGB2BGR)
+
+
+    red_channel = original_bgr[:, :, 2]
+
+    # Applica la maschera al canale rosso
+    red_channel = np.maximum(red_channel, mask)
+    # Apply morphological operations to remove small noise and fill gaps
+    # kernel = np.ones((2, 2), np.uint8)
+    # red_channel = cv2.erode(red_channel, kernel, iterations=1)
+    # red_channel = cv2.dilate(red_channel, kernel, iterations=1)
+    # Ricombina i canali dell'immagine
+    original_bgr[:, :, 2] = red_channel
+
+    # Draw bounding boxes on the original image
+
+    # for contour in contours:
+    #     if cv2.contourArea(contour) > 50:  # Filter small contours if needed
+    #         #print("Contour detected")
+    #         # cv2.drawContours(original_bgr, contour, -1, (0, 255, 0), 3)
+    #
+    #         x, y, w, h = cv2.boundingRect(contour)
+    #         cv2.rectangle(original_bgr, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    return original_bgr,mask
+
+
+def calculate_sensitivity(tp, fn):
+    """
+    Calculate sensitivity/ recall (True Positive Rate or Recall).
+    """
+    return (tp / (tp + fn))*100
+
+# Precision
+def calculate_precision(tp, fp):
+    """
+    Calculate Precision (True Positive Rate or Sensitivity).
+    """
+    return (tp / (tp + fp))*100
+
+
+def calculate_accuracy(tp, tn, fp, fn):
+    """
+    Calculate accuracy.
+    """
+    correct_predictions = tp + tn
+    total_predictions = tp + tn + fp + fn
+    return (correct_predictions / total_predictions)*100
+
+
+# def pixel_distance(output, target):
+#     '''
+#     Pixel distance between image1 and image2
+#     '''
+#     # Compute absolute difference between output and target images
+#     abs_diff = np.abs(output - target)
+#
+#     # Compute mean along channel dimension
+#     mean_diff = np.mean(abs_diff, axis=0)
+#
+#     # Add channel dimension
+#     distance_map = np.expand_dims(mean_diff, axis=0)
+#
+#     return distance_map
+def pixel_distance(output, target):
+    '''
+    Pixel distance between image1 and image2
+    '''
+    distance_map = torch.mean(torch.abs(output - target), dim=0).unsqueeze(0).unsqueeze(0)
+    return distance_map
+
+if __name__ == '__main__':
+    seed =1
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.benchmark = True
+    config_Dict = {"batch_size": 1,
+                   "epoch": 20,
+                   "lr": 5e-4,
+                   "z_dim": 512,
+                   "model_id": 'VAE_WITH_crazy_design',
+                   "tag": "long_run",
+                   "unet_inout_channels": 3,
+                   "unet_inplanes": 16,
+                   "unet_residual": 1,
+                   "unet_attention": [8],
+                   "unet_dropout": 0.0,
+                   "unet_mult": [1,2,3,4],
+                   "unet_resample": False,
+                   "unet_dims": 2,
+                   "unet_checkpoint": False,
+                   "unet_heads": 4,
+                   "unet_num_heads_channels": 4,
+                   "unet_res_updown": False,
+                   }
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #UNetModel
+    model = UNetModel_noskipp(in_channels=config_Dict['unet_inout_channels'],
+                      model_channels=config_Dict['unet_inplanes'],
+                      out_channels=config_Dict['unet_inout_channels'],
+                      num_res_blocks=config_Dict['unet_residual'],
+                      attention_resolutions=config_Dict['unet_attention'],
+                      dropout=config_Dict['unet_dropout'],
+                      channel_mult=config_Dict['unet_mult'],
+                      conv_resample=config_Dict['unet_resample'],
+                      dims=config_Dict['unet_dims'],
+                      use_checkpoint=config_Dict['unet_checkpoint'],
+                      num_heads=config_Dict['unet_heads'],
+                      num_head_channels =config_Dict['unet_num_heads_channels'],
+                      resblock_updown=config_Dict['unet_res_updown'],
+                      ).to(device)
+    # model = VAE_new(z_dim=config_dict["z_dim"]).to(device)
+    path_model = \
+        r"D:\github_directories\anomalyLocalization\checkpoints\20240613_130503_UNET_DESIGN_no_skip\20.pth"
+    model.load_state_dict(torch.load(path_model), strict=True)
+    test_loader = return_MVTecAD_loader_test_GN(
+            image_dir=r"D:\github_directories\foriegn\black_plate_c_shape_blue_pills\SEQ00004_MIXED_FOREIGN_PARTICLE",
+            batch_size=config_Dict["batch_size"])
+    model.eval()
+    output_sequence = []
+    output_sequence_reconstruced = []
+    output_sequence_ = []
+    invTrans = transforms.Compose([transforms.Normalize(mean=[0., 0., 0.],
+                                                        std=[1 / 0.0622145, 1 / 0.0864737, 1 / 0.07538847]),
+                                   transforms.Normalize(mean=[-0.09039213, -0.08525948, -0.09549119],
+                                                        std=[1., 1., 1.]),
+                                   ])
+    MSE = torch.nn.MSELoss(reduction='none')
+    gt_mask_list, seg_scores,anomaly_map_list = [],[],[]
+    for index,(x,mask_gn) in enumerate(tqdm(test_loader)):
+        with torch.no_grad():
+            x = x.to(device)
+            x_hat = model(x)
+            # x = invTrans(x.squeeze(0)).detach().cpu().numpy()
+            # x_output = invTrans(x_hat.squeeze(0)).detach().cpu().numpy()
+            mse = MSE(x,x_hat)
+            x = (x.squeeze(0)).detach().cpu().numpy()
+            x_output = (x_hat.squeeze(0)).detach().cpu().numpy()
+            # original = (x - x.min()) / (x.max() - x.min())
+            original = x
+            # reconstructed = (x_output - x_output.min()) / (x_output.max() - x_output.min())
+            reconstructed = x_output
+            new_frame,mask_calculated = plot_bounding_boxes(original=original,reconstructed=reconstructed,threshold=0.16)
+            original_tensor = torch.from_numpy(original)
+            reconstructed_tensor = torch.from_numpy(reconstructed)
+            # similar to diffusion model
+            # i_d = pixel_distance(reconstructed_tensor, reconstructed_tensor)
+            # sigma = 4
+            # kernel_size = 2 * int(4 * sigma + 0.5) + 1
+            # anomaly_map = gaussian_blur2d(i_d, kernel_size=(kernel_size, kernel_size), sigma=(sigma, sigma))
+            # anomaly_map_list.append(anomaly_map.squeeze(0).detach().cpu().numpy())
+            # End to diffusion model
+            seg_sig = 6
+            mask_calculated = np.expand_dims(mask_calculated, axis=0)
+            output_sequence_.append(mask_calculated.transpose(1,2,0))
+
+            # The purpose of applying the Gaussian filter in your code snippet seems to be to smooth the
+            # predicted masks before further processing or evaluation.
+            # for i in range(mask_calculated.shape[0]):
+            #     mask_calculated[i] = gaussian_filter(mask_calculated[i], sigma=seg_sig)
+
+            # mask_calculated = gaussian_filter(mask_calculated, sigma=seg_sig)
+            seg_scores.extend(mask_calculated)
+            gt_mask_list.extend(mask_gn.squeeze(0).cpu().numpy())
+
+            new_frame_reconstruced = cv2.cvtColor((reconstructed.transpose(1, 2, 0)*255).astype(np.uint8),
+                                       cv2.COLOR_RGB2BGR)
+
+            output_sequence_reconstruced.append(new_frame_reconstruced)
+            output_sequence.append(new_frame)
+
+    seg_scores = np.asarray(seg_scores).astype(np.float32)
+    seg_scores /= 255.0  # Normalize to range [0, 1]
+    seg_scores = seg_scores.astype(np.uint8)
+    # anomaly_map_list = (np.asarray(anomaly_map_list)).astype(np.uint8)
+    # anomaly_map_list = (
+    #             (anomaly_map_list - anomaly_map_list.min()) / (anomaly_map_list.max() - anomaly_map_list.min())).astype(np.uint8)
+
+    gt_mask = (np.asarray(gt_mask_list)).astype(np.uint8)
+
+    # Flatten the arrays if needed
+    gt_flat = gt_mask.flatten()
+    pred_flat = seg_scores.flatten()
+    print("Ground Truth shape",gt_flat.shape)
+    print("Predicted shape",pred_flat.shape)
+    # Calculate confusion matrix
+    conf_matrix = confusion_matrix(gt_flat, pred_flat)
+    goal_matrix = confusion_matrix(gt_flat, gt_flat)
+    print("Target Goal")
+    print(goal_matrix)
+    print("algo performance")
+    print(conf_matrix)
+
+
+
+    # True positives (TP) are in the intersection of positive class in both gt and predictions
+    true_positives = conf_matrix[1, 1]
+    true_negatives = conf_matrix[0, 0]
+
+    # False positives (FP) are positive in predictions but negative in gt
+    false_positives = conf_matrix[0, 1]
+    false_negatives = conf_matrix[1, 0]
+
+    perc_matrix = np.array([[(conf_matrix[0, 0]/goal_matrix[0,0])*100,
+                            -1],
+                           [-1,
+                            (conf_matrix[1, 1]/goal_matrix[1,1])*100]])
+
+    print("perc_matrix")
+    print(perc_matrix)
+
+    # Average number of true positive activations per pixel
+    average_true_positive = true_positives / np.sum(gt_flat == 1)
+
+    # Average number of false positive activations per pixel
+    # we divide the false postive by the total number no activations
+    average_false_positive = false_positives / np.sum(gt_flat == 0)
+    print('average true postive: {0}'.format(average_true_positive))
+    print('average false postive: {0}'.format(average_false_positive))
+
+    print("The Sensetivity/Recall is: {0}".format(calculate_sensitivity(true_positives,false_negatives)))
+    print("The Precsion is: {0}".format(calculate_precision(true_positives,false_positives)))
+    print("The accuracy is: {0}".format(calculate_accuracy(true_positives,
+                                                           true_negatives,
+                                                           false_positives,
+                                                           false_negatives)))
+    # per_pixel_rocauc = roc_auc_score(gt_mask.flatten(), seg_scores.flatten())
+    per_pixel_rocauc = roc_auc_score(gt_mask.flatten(), seg_scores.flatten())
+
+    print('pixel ROCAUC: %.2f' % (per_pixel_rocauc))
+    makeVideoFromImageArray_uncolored('mask.avi', output_sequence_)
+    makeVideoFromImageArray('video.avi', output_sequence)
+    makeVideoFromImageArray('video_constructed.avi', output_sequence_reconstruced)
